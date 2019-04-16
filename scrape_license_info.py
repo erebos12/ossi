54becdc51fd1d886a06c802b5581fe2dae427300
Alex Sahm <sahm.alexander@pwc.com>
2019-04-16 16:00:19 +0200

    get the table as pandas data frame

--- scrape_license_info.py
+++ scrape_license_info.py
@@ -1,20 +1,61 @@
 import requests
-from bs4 import BeautifulSoup as soup
+import pandas as pd
+from bs4 import BeautifulSoup
 
 url = 'https://en.wikipedia.org/wiki/Comparison_of_free_and_open-source_software_licenses'
 
-raw_html = requests.get(url).text
 
-html = soup(raw_html, 'lxml')
+class HTMLTableParser:
 
-table = soup.find_all(html)[0]  # Grab the first table
+    def parse_url(self, url):
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        table = soup.find_all('table')[0]
+        return self.parse_html_table(table)
 
-output_rows = []
-for table_row in table.findAll('tr'):
-    columns = table_row.findAll('td')
-    output_row = []
-    for column in columns:
-        output_row.append(column.text)
-    output_rows.append(output_row)
+    def parse_html_table(self, table):
+        n_columns = 0
+        n_rows = 0
+        column_names = []
 
-print(output_rows)
+        # Find number of rows and columns
+        # we also find the column titles if we can
+        for row in table.find_all('tr'):
+
+            # Determine the number of rows in the table
+            td_tags = row.find_all('td')
+            if len(td_tags) > 0:
+                n_rows += 1
+                if n_columns == 0:
+                    # Set the number of columns for our table
+                    n_columns = len(td_tags)
+
+            # Handle column names if we find them
+            th_tags = row.find_all('th')
+            if len(th_tags) > 0 and len(column_names) == 0:
+                for th in th_tags:
+                    column_names.append(th.get_text())
+
+        # Safeguard on Column Titles
+        if len(column_names) > 0 and len(column_names) != n_columns:
+            raise Exception("Column titles do not match the number of columns")
+
+        columns = column_names if len(column_names) > 0 else range(0, n_columns)
+        df = pd.DataFrame(columns=columns,
+                          index=range(0, n_rows))
+        row_marker = 0
+        for row in table.find_all('tr'):
+            column_marker = 0
+            columns = row.find_all('td')
+            for column in columns:
+                df.iat[row_marker, column_marker] = column.get_text()
+                column_marker += 1
+            if len(columns) > 0:
+                row_marker += 1
+
+        return df
+
+
+hp = HTMLTableParser()
+table = hp.parse_url(url)  # Grabbing the table from the tuple
+print(table)
