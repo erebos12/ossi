7e897ead5a84c2e67f8d8268751c6051f661409c
Alex Sahm <sahm.alexander@pwc.com>
2019-04-17 14:53:12 +0200

    new html table to dict parsing

--- infrastructure/html_table_parser.py
+++ infrastructure/html_table_parser.py
@@ -1,8 +1,39 @@
 from bs4 import BeautifulSoup
 
+
 def get_all_tables_from_html(html):
     try:
         soup = BeautifulSoup(html, 'lxml')
         return list(soup.find_all('table'))
     except Exception as e:
         raise e
+
+
+def parse_table(html_table):
+    table_data = []
+    header_list = []
+    for row in html_table.find_all('tr'):
+        if not header_list:
+            header_list = __get_header_data(row)
+        items = __get_row_data(row)
+        if (items and header_list) and (len(items) == len(header_list)):
+            table_data.append(__zip_it(header_list, items))
+    return table_data
+
+
+def __get_row_data(html_row):
+    data = html_row.find_all('td')
+    return __get_data_as_list(data)
+
+
+def __get_header_data(html_row):
+    headers = html_row.find_all('th')
+    return __get_data_as_list(headers)
+
+
+def __get_data_as_list(data):
+    return [d.get_text() for d in data if d.get_text()]
+
+
+def __zip_it(keys, values):
+    return dict(zip(keys, values))
--- unittests/test_html_table_parsing.py
+++ unittests/test_html_table_parsing.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 
-from infrastructure.html_table_parser import get_all_tables_from_html
+from infrastructure.html_table_parser import get_all_tables_from_html, parse_table
 from infrastructure.scrape_license_info import HTMLTableParser
 
 
@@ -32,3 +32,15 @@ class TestParseHTMLTable(unittest.TestCase):
         self.assertEqual("Eve", table_as_df['Firstname'].iloc[1])
         self.assertEqual("Smith", table_as_df['Lastname'].iloc[0])
         self.assertEqual("Jackson", table_as_df['Lastname'].iloc[1])
+
+
+    def test_parse_html_table(self):
+        html = get_html_from_file('sample_html_table.html')
+        tables = get_all_tables_from_html(html)
+        tbl_as_dict_list = parse_table(tables[0])
+        self.assertEqual(2, len(tbl_as_dict_list))
+        self.assertEqual("Jill", tbl_as_dict_list[0]['Firstname'])
+        self.assertEqual("Eve", tbl_as_dict_list[1]['Firstname'])
+        self.assertEqual("Smith", tbl_as_dict_list[0]['Lastname'])
+        self.assertEqual("Jackson", tbl_as_dict_list[1]['Lastname'])
+
