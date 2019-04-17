34f9ce7d53eb8116cfbd8c62e1f6664cefe2902a
Alex Sahm <sahm.alexander@pwc.com>
2019-04-17 14:52:54 +0200

    removed comments

--- infrastructure/scrape_license_info.py
+++ infrastructure/scrape_license_info.py
@@ -17,26 +17,17 @@ class HTMLTableParser:
         n_columns = 0
         n_rows = 0
         column_names = []
-
-        # Find number of rows and columns
-        # we also find the column titles if we can
         for row in table.find_all('tr'):
-
-            # Determine the number of rows in the table
             td_tags = row.find_all('td')
             if len(td_tags) > 0:
                 n_rows += 1
                 if n_columns == 0:
-                    # Set the number of columns for our table
                     n_columns = len(td_tags)
-
-            # Handle column names if we find them
             th_tags = row.find_all('th')
             if len(th_tags) > 0 and len(column_names) == 0:
                 for th in th_tags:
                     column_names.append(th.get_text())
 
-        # Safeguard on Column Titles
         if len(column_names) > 0 and len(column_names) != n_columns:
             raise Exception("Column titles do not match the number of columns")
 
